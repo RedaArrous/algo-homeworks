3c47a58286a3c491aa37a900207e14f7b5f38142
reda <hissocka2006@gmail.com>
2024-11-01 10:07:11 +0100

    commiting

--- algo-td/Seires-01/exercise-04/04.c
+++ algo-td/Seires-01/exercise-04/04.c
@@ -9,21 +9,27 @@ int main() {
 	scanf("%lf%lf%lf", &a, &b, &c);
 	printf("the equation is: %lfx^2 + %lfx + %lf\n", a, b, c);
 	deltaa = (b * b) - (4 * a * c);
-	if (deltaa > 0)
-	{
-		x = (-b + sqrt(deltaa)) / (2 * a);
-		printf("we have x1 = %lf\n", x);
-		x = (-b - sqrt(deltaa)) / (2 * a);
-		printf("and x2 = %lf", x);
-	}
-	else if (deltaa == 0) {
-		x = -b / (2 * a);
-		printf("we have double roote x1  = %lf", x);
-	}
-	else {
-		printf("there is no solution for the equation");
-	}
+	if (a != 0 && b !=0 && c != 0) {
+            if (deltaa > 0)
+        {
+            x = (-b + sqrt(deltaa)) / (2 * a);
+            printf("we have x1 = %lf\n", x);
+            x = (-b - sqrt(deltaa)) / (2 * a);
+            printf("and x2 = %lf", x);
+        }
+        else if (deltaa == 0) {
+            x = -b / (2 * a);
+            printf("we have double roote x1  = %lf", x);
+        }
+        else {
+            printf("there is no solution for the equation");
+        }
+    }
+    else{
+        printf("this is not an equation, 0 = 0");
+    }
 }
+
 /* you can try this:
 a = 1, b = 0, c = -4;
 a = 1, b = 5, c = 6;
--- algo-td/Seires-01/exercise-06/06.c
+++ algo-td/Seires-01/exercise-06/06.c
@@ -6,7 +6,7 @@ int main() {
     printf("please enter a day, month and a year: \n");
     scanf("%d %d %d", &d, &m, &y);
     //checking the year and months and for the days it's enough to check if they're positive because we'll check them again later in the program
-    if (y >= 0 && y <= 2024 && m >= 1 && m <= 12 && d >= 1) {
+    if (y >= 0 && y <= 9999 && m >= 1 && m <= 12 && d >= 1) {
         // condition for february in leap year, if it's feruary and the year is either divisible by 4 and not by 100 or divisible by 400
         if (m == 2 && ( (y % 4) == 0 && (y % 100) != 0 || (y % 400) == 0) && d <= 29) {
             printf("%d/%d/%d\nthe date is validdd",d, m, y);
